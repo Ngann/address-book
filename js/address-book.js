c084fe421117f1cc7e15454035d5ef4d94b3084f
Jacey Tupa and Ngan Nguyen <jacey@>
2018-10-29 13:20:49 -0700

    Add UI Logic to address-book.js and block elements to index.html

---
+++ js/address-book.js
@@ -0,0 +1,99 @@
+// Business Logic for AddressBook -------------------------
+function AddressBook () {
+  this.contacts = [],
+  this.currentId = 0
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id === id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.updateContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id === id) {
+        return this.contacts[i].update();
+      }
+    }
+  };
+  return false;
+}
+
+
+//Business Logic for Contacts ------------------------------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName,
+  this.lastName = lastName,
+  this.phoneNumber = phoneNumber
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+Contact.prototype.update = function() {
+  return this.firstName = "test";
+}
+
+
+// var contact = new Contact("Ada", "Lovelace", "503-555-0100");
+// var contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+// var contact3 = new Contact("Jacey", "Tupa", "503-555-0100");
+// var contact4 = new Contact("Ngan", "Nguyen", "503-555-0199");
+// addressBook.addContact(contact);
+// addressBook.addContact(contact2);
+// addressBook.addContact(contact3);
+// addressBook.addContact(contact4);
+
+//User Interface Logic ----------------------------------------
+var addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  var contactsList = $("ul#contacts");
+  var htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  })
+})
